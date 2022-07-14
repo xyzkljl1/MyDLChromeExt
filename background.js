723097e19991accf8000ac5cd3c2091bd5ae5563
xyzkljl1 <xyzkljl1@yahoo.com>
2022-07-14 20:20:11 +0800

    修复发送的信息不全的bug

--- background.js
+++ background.js
@@ -17,10 +17,9 @@ chrome.contextMenus.onClicked.addListener(function (info, tab) {
                         chrome.cookies.getAll({ "url": tab.url }, function (cookies) {
                             var ret = "";
                             for (let cookie of cookies)
-                                if (cookie.name == "csrftoken" || cookie.name == "sessionid")//nhentai
-                                    ret += cookie.name + "=" + cookie.value + "; ";
-                            //console.log(ret);
-                            fetch('http://127.0.0.1:4000/?' + 'url=' + tab.url + "&" + "cookie=" + ret, {
+                                if (cookie.name == "csrftoken" || cookie.name == "sessionid" || cookie.name == "cf_clearance")//nhentai
+                                    ret += cookie.name + "=" + cookie.value + ";";
+                            fetch('http://127.0.0.1:4000/?' + 'url=' + encodeURIComponent(tab.url) + "&" + "cookie=" + encodeURIComponent(ret), {
                                 method: 'GET',
                                 headers: { 'Cache-Control': 'no-cache' }
                             }).then(res => res.text()).then(function (result) {
@@ -33,8 +32,8 @@ chrome.contextMenus.onClicked.addListener(function (info, tab) {
                         var ret = "";
                         for (let cookie of cookies)
                             if (cookie.name == "csrftoken" || cookie.name == "sessionid")
-                                ret += cookie.name + "=" + cookie.value + "; ";
-                        fetch('http://127.0.0.1:4000/?' + 'url=' + tab.url + "&" + "cookie=" + ret,{
+                                ret += cookie.name + "=" + cookie.value + ";";
+                        fetch('http://127.0.0.1:4000/?' + 'url=' + encodeURIComponent(tab.url) + "&" + "cookie=" + encodeURIComponent(ret),{
                             method: 'GET',
                             headers: { 'Cache-Control': 'no-cache' }
                         }).done(function (result) {
